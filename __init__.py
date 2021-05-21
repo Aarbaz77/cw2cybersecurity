b2980fc2d65b908ec0bc4e6ba953d1096a561e47
Aarbaz Alam <aarbazlm6@gmail.com>
2021-05-21 12:25:17 +0100

    Final Changes

--- __init__.py
+++ __init__.py
@@ -17,7 +17,7 @@ def create_app():
     db.init_app(app)
 
     login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
+    login_manager.login_view = 'views.login'
     login_manager.init_app(app)
 
     from .models import User
@@ -26,10 +26,7 @@ def create_app():
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    from .auth import auth as auth_blueprint
+    from .views import views as auth_blueprint
     app.register_blueprint(auth_blueprint)
-
-    from .main import main as main_blueprint
-    app.register_blueprint(main_blueprint)
 
     return app
--- views.py
+++ views.py
@@ -4,13 +4,17 @@ from flask_login import login_user, logout_user, login_required, current_user
 from .models import User, inventory
 from . import db
 
-auth = Blueprint('auth', __name__)
+views = Blueprint('views', __name__)
 
-@auth.route('/login')
+@views.route('/')
+def index():
+    return render_template('index.html')
+
+@views.route('/login')
 def login():
     return render_template('login.html')
 
-@auth.route('/login', methods=['POST'])
+@views.route('/login', methods=['POST'])
 def login_post():
     email = request.form.get('email')
     password = request.form.get('password')
@@ -20,21 +24,21 @@ def login_post():
 
     if not user:
         flash('Please check your login details and try again.')
-        return redirect(url_for('auth.login'))
+        return redirect(url_for('views.login'))
     elif user and not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
-        return redirect(url_for('auth.login'))
+        return redirect(url_for('views.login'))
 
 
     login_user(user, remember=remember)
 
-    return redirect(url_for('main.profile'))
+    return redirect(url_for('views.profile'))
 
-@auth.route('/signup')
+@views.route('/signup')
 def signup():
     return render_template('signup.html')
 
-@auth.route('/signup', methods=['POST'])
+@views.route('/signup', methods=['POST'])
 def signup_post():
     email = request.form.get('email')
     name = request.form.get('name')
@@ -44,40 +48,51 @@ def signup_post():
 
     if user:
         flash('Email address already exists.')
-        return redirect(url_for('auth.signup'))
+        return redirect(url_for('views.signup'))
 
     new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
 
     db.session.add(new_user)
     db.session.commit()
 
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('views.login'))
 
-@auth.route('/logout')
+@views.route('/logout')
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('main.index'))
+    return redirect(url_for('views.index'))
 
-@auth.route('/inventorypage')
+@views.route('/profile')
+@login_required
+def profile():
+    user = current_user
+    items = inventory.query.filter_by(seller_id=user.id)
+    return render_template('profile.html', name=user.name, items = items)
+
+@views.route('/inventorypage')
 def inventorypage():
     items = inventory.query.all()
     print(items)
     return render_template('inventory.html', items = items)
 
-@auth.route('/inventorycreate')
+@views.route('/inventorycreate')
 @login_required
 def inventorycreatepage():
     return render_template('createinventory.html')
 
-@auth.route('/selfitems')
+@views.route('/inventorycreate', methods=["POST"])
 @login_required
-def selfitems():
+def inventorycreate():
+    item = request.form.get('name')
+    price = request.form.get('price')
     user = current_user
-    items = inventory.query.filter_by(seller_id=user.id)
-    return render_template('selfitems.html', items = items)
+    inventoryitem = inventory(name=item, price=price, seller_id=user.id)
+    db.session.add(inventoryitem)
+    db.session.commit()
+    return redirect(url_for('views.inventorypage'))
 
-@auth.route('/updateitems/<int:nid>', methods=['POST','GET'])
+@views.route('/updateitems/<int:nid>', methods=['POST','GET'])
 @login_required
 def updateitems(nid):
     user = current_user
@@ -89,14 +104,14 @@ def updateitems(nid):
         if (item_update.seller_id==current_user.id):
             db.session.commit()
             flash('Succesfully Updates ' + item_name)
-            return redirect(url_for('auth.selfitems'))
+            return redirect(url_for('views.profile'))
         else:
             flash('Unable to update ' + item_name)
-            return redirect(url_for('auth.selfitems'))
+            return redirect(url_for('views.profile'))
     else:
         return render_template('updateinventory.html', items = item_update)
 
-@auth.route('/delete/<int:nid>')
+@views.route('/delete/<int:nid>')
 @login_required
 def deleteitems(nid):
     print(nid)
@@ -104,25 +119,11 @@ def deleteitems(nid):
     item_delete = inventory.query.get_or_404(nid)
     print(item_delete)
     item_name = item_delete.name
-    # db.session.delete(item_delete)
-    # flash('Succesful')
-    # return redirect(url_for('auth.selfitems'))
     if(item_delete.seller_id==current_user.id):
         db.session.delete(item_delete)
         db.session.commit()
         flash('Succesfully Deleted ' + item_name)
-        return redirect(url_for('auth.selfitems'))
+        return redirect(url_for('views.profile'))
     else:
         flash('UnSuccesful')
-        return redirect(url_for('auth.selfitems'))
-
-@auth.route('/inventorycreate', methods=["POST"])
-@login_required
-def inventorycreate():
-    item = request.form.get('name')
-    price = request.form.get('price')
-    user = current_user
-    inventoryitem = inventory(name=item, price=price, seller_id=user.id)
-    db.session.add(inventoryitem)
-    db.session.commit()
-    return redirect(url_for('auth.inventorypage'))
+        return redirect(url_for('views.profile'))
